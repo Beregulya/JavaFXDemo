730a5a17a799ac4f6ecba713f4c2a0f13adc9add
Beregulya <beregulya.dev@gmail.com>
2017-01-24 17:35:07 +0200

    Switching Scenes

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application  {
 
-    Button button;
+    Stage window;
+    Scene scene1, scene2;
 
     public static void main(String[] args) {
         launch(args);
@@ -16,27 +19,26 @@ public class Main extends Application  {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
 
-        primaryStage.setTitle("Заголовок окна");
+        Label label1 = new Label("Добро пожаловать! Это сцена номер один.");
+        Button button1 = new Button("Отправиться к сцене номер два...");
+        button1.setOnAction(e -> window.setScene(scene2));
 
-        button = new Button("Эй, крошка...");
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 500, 500);
 
-        button.setOnAction(e -> System.out.println("Руки свои убрал!"));
+        Button button2 = new Button("Это сцена полный отстой, вернуться обратно.");
+        button2.setOnAction(e -> window.setScene(scene1));
 
-        /*button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Не дави на меня!");
-            }
-        });*/
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 300);
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-
-        Scene scene = new Scene(layout, 300, 250);
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        window.setScene(scene1);
+        window.setTitle("Заголовок");
+        window.show();
 
     }
 
