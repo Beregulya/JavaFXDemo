daa3a8f686140c56d0d7b6efc7d532512bb84ad3
Beregulya <beregulya.dev@gmail.com>
2017-01-25 22:54:12 +0200

    Simple TableView

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import com.sun.javafx.scene.control.skin.ChoiceBoxSkin;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
@@ -14,7 +16,7 @@ import java.util.Observable;
 public class Main extends Application  {
 
     Stage window;
-    TreeView<String> tree;
+    TableView<Product> table;
 
     public static void main(String[] args) {
         launch(args);
@@ -25,17 +27,38 @@ public class Main extends Application  {
         window = primaryStage;
         window.setTitle("Demo");
 
-        StackPane layout = new StackPane();
-        Scene scene = new Scene(layout, 300, 250);
+        TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setMinWidth(200);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Product, Double> priceColumn = new TableColumn<>("Price");
+        priceColumn.setMinWidth(100);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        TableColumn<Product, Integer> quantityColumn = new TableColumn<>("Quantity");
+        quantityColumn.setMinWidth(100);
+        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+
+        table = new TableView<>();
+        table.setItems(getProduct());
+        table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(table);
+
+        Scene scene = new Scene(vBox);
         window.setScene(scene);
         window.show();
     }
 
-    private TreeItem<String> makeBranch(String title, TreeItem<String> parent) {
-        TreeItem<String> item = new TreeItem<>(title);
-        item.setExpanded(true);
-        parent.getChildren().add(item);
-        return item;
+    public ObservableList<Product> getProduct() {
+        ObservableList<Product> products = FXCollections.observableArrayList();
+        products.add(new Product("Laptop", 859.00, 10));
+        products.add(new Product("Ball", 9.00, 36));
+        products.add(new Product("Book", 24.99, 20));
+        products.add(new Product("Car", 1999.49, 1));
+        products.add(new Product("Cigarette", 3.00, 48));
+        return products;
     }
 
 }
