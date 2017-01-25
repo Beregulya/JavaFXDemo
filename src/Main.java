74b5ee13e628ebcd3ace6ba61a675d362878d563
Beregulya <beregulya.dev@gmail.com>
2017-01-25 14:36:58 +0200

    ChoiceBox

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
+import com.sun.javafx.scene.control.skin.ChoiceBoxSkin;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
@@ -24,34 +22,30 @@ public class Main extends Application  {
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
         window.setTitle("Demo");
+        button = new Button("Click me");
 
-        CheckBox box1 = new CheckBox("Bacon");
-        CheckBox box2 = new CheckBox("Tuna");
-        box2.setSelected(true);
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
 
-        button = new Button("Order now");
-        button.setOnAction(e -> handleOptions(box1, box2));
+        choiceBox.getItems().add("Apples");
+        choiceBox.getItems().add("Bananas");
+        choiceBox.getItems().addAll("Bacon", "Ham", "Meatballs");
+
+        choiceBox.setValue("Apples");
+
+        button.setOnAction(e -> getChoice(choiceBox));
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(box1, box2, button);
+        layout.getChildren().addAll(choiceBox, button);
 
-        scene = new Scene(layout, 350, 200);
+        scene = new Scene(layout, 300, 250);
         window.setScene(scene);
-
         window.show();
     }
 
-    private void handleOptions(CheckBox box1, CheckBox box2) {
-        String message = "User`s order:\n";
-
-        if (box1.isSelected())
-            message += "Bacon\n";
-
-        if (box2.isSelected())
-            message += "Tuna\n";
-
-        System.out.println(message);
+    private void getChoice(ChoiceBox<String> choiceBox) {
+        String food = choiceBox.getValue();
+        System.out.println(food);
     }
 
 }
